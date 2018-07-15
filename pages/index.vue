5c9dab094b30ab86552d7925fd0a5eb9b1d230a8
Ben Layer <benlayer291@gmail.com>
2018-07-15 23:46:08 +0100

    Add Hero placeholder to homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,64 +1,19 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        break-journal
-      </h1>
-      <h2 class="subtitle">
-        A journal site built with Nuxt.js
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+  <main>
+    <Hero
+      :fields="{
+        title: 'Break'
+      }"
+    />
+  </main>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+import Hero from '@/components/Hero.vue'
 
 export default {
   components: {
-    AppLogo,
+    Hero,
   },
 }
 </script>
-
-<style>
-.container {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  min-height: 100vh;
-  text-align: center;
-}
-
-.title {
-  display: block;
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  font-size: 100px;
-  font-weight: 300;
-  letter-spacing: 1px;
-  color: #35495e;
-}
-
-.subtitle {
-  font-size: 42px;
-  font-weight: 300;
-  padding-bottom: 15px;
-  color: #526488;
-  word-spacing: 5px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
