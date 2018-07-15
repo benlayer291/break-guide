6cb33e28543fafc5bfff3203d410700a1115b871
Ben Layer <benlayer291@gmail.com>
2018-07-15 16:16:52 +0100

    Add css plugins and configure postcss-env to use stage 1 to enable custom media queries

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,8 +18,11 @@ module.exports = {
     },
     // Process css with postcss plugins
     postcss: [
-      require('postcss-import'),
+      require('postcss-import')(),
+      require('postcss-calc')(),
+      require('postcss-nested')(),
       require('postcss-preset-env')({
+        stage: 1,
         browsers: ['last 2 versions', 'ie >= 11'],
       }),
     ],
