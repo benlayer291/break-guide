b26590b4c594da8dbcc5009d140920562a0c60d4
Ben Layer <benlayer291@gmail.com>
2018-07-15 16:46:59 +0100

    Create placeholder break-article page

---
+++ pages/break/_slug/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <Hero
+    :fields="{
+      title: 'Article'
+    }"
+  />
+</template>
+
+<script>
+import Hero from '@/components/Hero'
+
+export default {
+  components: {
+    Hero,
+  },
+}
+</script>
