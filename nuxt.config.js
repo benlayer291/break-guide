57283baa85ccc8b6e5be0e542e92ae2194910ec6
Ben Layer <benlayer291@gmail.com>
2018-07-22 14:48:29 +0100

    Add meta data and favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -91,9 +91,9 @@ module.exports = {
       require('postcss-nested')(),
       require('postcss-preset-env')({
         stage: 1,
-        browsers: ['last 2 versions', 'ie >= 11']
-      })
-    ]
+        browsers: ['last 2 versions', 'ie >= 11'],
+      }),
+    ],
   },
   /*
   ** CSS
@@ -121,24 +121,180 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'break-guide',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'description',
-        name: 'description',
-        content: 'A journal site built with Nuxt.js',
-      },
-    ],
+    title: 'Break Guide',
     link: [
       {
         rel: 'stylesheet',
         href:
           'https://fonts.googleapis.com/css?family=Abril+Fatface|Anonymous+Pro'
       },
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png',
+      },
+      {
+        rel: 'manifest',
+        href: '/site.webmanifest',
+      },
+      {
+        rel: 'mask-icon',
+        href: '/safari-pinned-tab.svg',
+        color: '#000000',
+      },
+      {
+        rel: 'dns-prefetch',
+        href: 'cdn.contentful.com',
+      },
+      {
+        rel: 'dns-prefetch',
+        href: 'break-guide.netlify.com',
+      },
+      {
+        rel: 'dns-prefetch',
+        href: 'images.ctfassets.net',
+      },
+      {
+        rel: 'dns-prefetch',
+        href: 'www.google-analytics.com',
+      },
     ],
+    meta: [
+      {
+        charset: 'utf-8',
+      },
+      {
+        name: 'msapplication-TileColor',
+        content: '#ffffff',
+      },
+      {
+        name: 'theme-color',
+        content: '#ffffff',
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
+      {
+        hid: 'title',
+        name: 'title',
+        content: 'Break Guide',
+      },
+      {
+        content: 'Break – lunch, coffee, city breaks. Curating travel, culture, & food.',
+        hid: 'description',
+        name: 'description',
+      },
+      {
+        content: 'Break Guide',
+        name: 'dcterms.Contributor',
+      },
+      {
+        content: 'https://break-guide.netlify.com',
+        name: 'dcterms.Coverage',
+      },
+      {
+        content: 'Break Guide',
+        name: 'dcterms.Creator',
+      },
+      {
+        content: 'Break – lunch, coffee, city breaks. Curating travel, culture, & food.',
+        name: 'dcterms.Description',
+        hid: 'dcterms.Description',
+      },
+      {
+        content: 'text/html',
+        name: 'dcterms.Format',
+      },
+      {
+        content: 'https://break-guide.netlify.com',
+        name: 'dcterms.Identifier',
+      },
+      {
+        content: 'en_GB',
+        name: 'dcterms.Language',
+      },
+      {
+        content: 'Break Guide',
+        name: 'dcterms.Publisher',
+      },
+      {
+        content: 'Break Guide',
+        hid: 'dcterms.Title',
+        name: 'dcterms.Title',
+      },
+      {
+        content: 'website',
+        name: 'dcterms.Type',
+      },
+      {
+        content: 'Break – lunch, coffee, city breaks. Curating travel, culture, & food.',
+        hid: 'og:description',
+        name: 'og:description',
+      },
+      {
+        content: '/imgs/social-default.jpg',
+        hid: 'og:image',
+        name: 'og:image',
+      },
+      {
+        content: 'en_GB',
+        name: 'og:locale',
+      },
+      {
+        content: 'https://www.instagram.com/break_guide/',
+        name: 'og:see_also',
+      },
+      {
+        content: 'Break Guide',
+        name: 'og:site_name',
+      },
+      {
+        content: 'Break Guide',
+        hid: 'og:title',
+        name: 'og:title',
+      },
+      {
+        content: 'website',
+        name: 'og:type',
+      },
+      {
+        content: 'https://break-guide.netlify.com',
+        name: 'og:url',
+      },
+      {
+        content: 'summary_large_image',
+        name: 'twitter:card',
+      },
+      {
+        content: 'Break – lunch, coffee, city breaks. Curating travel, culture, & food.',
+        hid: 'twitter:description',
+        name: 'twitter:description',
+      },
+      {
+        content: '/imgs/social-default.jpg',
+        hid: 'twitter:image',
+        name: 'twitter:image',
+      },
+      {
+        content: 'Break Guide',
+        hid: 'twitter:title',
+        name: 'twitter:title',
+      },
+    ],
+    titleTemplate: '%s - break guide',
   },
   /*
   ** Customize the progress bar color
